2a972c02a123299972f3685aeaf81c1aa7d850ad
erane <eranyo@mta.ac.il>
2023-01-18 00:41:05 +0200

    new

---
+++ server-sources.js
@@ -0,0 +1,131 @@
+names = {
+    'Eran Yosefia': {
+        attendance: false,
+        code: '5e895f892wr',
+        partner: 'Ohad Segal'
+    },
+    'Ohad Segal': {
+        attendance: false,
+        code: '5e895f892am',
+        partner: 'Eran Yosefia'
+    },
+    'Adi Elron': {
+        attendance: false,
+        code: '5e895f892rj',
+        partner: 'Ohad Segal'
+    },
+    'Amit Eilam': {
+        attendance: false,
+        code: '5e895f892ro',
+        partner: 'Ohad Segal'
+    },
+    'Assaf Gofer': {
+        attendance: false,
+        code: '5e895f892io',
+        partner: 'Ohad Segal'
+    },
+    'Ben Cohen': {
+        attendance: false,
+        code: '5e895f892re',
+        partner: 'Ohad Segal'
+    },
+    'Benjamin Harrosh': {
+        attendance: false,
+        code: '5e895f892hg',
+        partner: 'Ohad Segal'
+    },
+    'Dana Torgo': {
+        attendance: false,
+        code: '5e895f892hy',
+        partner: 'Ohad Segal'
+    },
+    'Dor Oren': {
+        attendance: false,
+        code: '5e895f892qw',
+        partner: 'Ohad Segal'
+    },
+    'Eden Azulay': {
+        attendance: false,
+        code: '5e895f892er',
+        partner: 'Ohad Segal'
+    },
+    'Gal Baron': {
+        attendance: false,
+        code: '5e895f892tl',
+        partner: 'Ohad Segal'
+    },
+    'Hen Sinai': {
+        attendance: false,
+        code: '5e895f892zx',
+        partner: 'Ohad Segal'
+    },
+    'Ofri Zadok': {
+        attendance: false,
+        code: '5e895f892xc',
+        partner: 'Ohad Segal'
+    },
+    'Ron Hachmon': {
+        attendance: false,
+        code: '5e895f892cv',
+        partner: 'Ohad Segal'
+    },
+    'Snir Pinievsky': {
+        attendance: false,
+        code: '5e895f892vb',
+        partner: 'Ohad Segal'
+    },
+    'Stav Cohen': {
+        attendance: false,
+        code: '5e895f892bn',
+        partner: 'Ohad Segal'
+    },
+    'Tal Birs': {
+        attendance: false,
+        code: '5e895f892az',
+        partner: 'Ohad Segal'
+    },
+    'Tomer Klein': {
+        attendance: false,
+        code: '5e895f892am',
+        partner: 'Ohad Segal'
+    },
+    'Yuval Reish': {
+        attendance: false,
+        code: '5e895f892ap',
+        partner: 'Ohad Segal'
+    },
+    'Ido Yekutiel': {
+        attendance: false,
+        code: '5e895f892up',
+        partner: 'Ohad Segal'
+    },
+    'Lir Mimrod': {
+        attendance: false,
+        code: '5e895f892pf',
+        partner: 'Ohad Segal'
+    },
+    'Nave Maor': {
+        attendance: false,
+        code: '5e895f892pz',
+        partner: 'Ohad Segal'
+    },
+    'Idan Zimi': {
+        attendance: false,
+        code: '5e895f892mz',
+        partner: 'Ohad Segal'
+    },
+    'Idan Cohen': {
+        attendance: false,
+        code: '5e895f892sp',
+        partner: 'Ohad Segal'
+    },
+    'Adi Rozenberg': {
+        attendance: false,
+        code: '5e895f892zi',
+        partner: 'Ohad Segal'
+    },
+}
+
+module.exports = {
+    names
+}
